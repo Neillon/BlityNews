8f7aea79cfdd1e8180fa9d8b023adba8644dba88
neillon <neilloncesar13@gmail.com>
2021-04-03 14:53:06 -0300

    Adding ktlint and detekt

---
+++ README.md
@@ -0,0 +1,4 @@
+[![ktlint](https://img.shields.io/badge/code%20style-%E2%9D%A4-FF4081.svg)](https://ktlint.github.io/)
+
+# BlityNews
+The best news app ever maded
--- app/src/main/java/com/blityapps/blitynews/theming/BlityNewsColors.kt
+++ app/src/main/java/com/blityapps/blitynews/theming/BlityNewsColors.kt
@@ -6,14 +6,21 @@ import androidx.compose.ui.graphics.Color
 
 object BlityNewsColors {
 
+    private const val COLOR_ERROR = 0xFFB00020
+
+    private const val COLOR_PRIMARY_LIGHT = 0xFF3a9cff
+    private const val COLOR_PRIMARY_VARIANT_LIGHT = 0xFF006ecb
+    private const val COLOR_SECONDARY_LIGHT = 0xFFffe0b4
+    private const val COLOR_SECONDARY_VARIANT_LIGHT = 0xFFcbae84
+
     val lightColors = lightColors(
-        primary = Color(0xFF3a9cff),
-        primaryVariant = Color(0xFF006ecb),
-        secondary = Color(0xFFffe0b4),
-        secondaryVariant = Color(0xFFcbae84),
+        primary = Color(COLOR_PRIMARY_LIGHT),
+        primaryVariant = Color(COLOR_PRIMARY_VARIANT_LIGHT),
+        secondary = Color(COLOR_SECONDARY_LIGHT),
+        secondaryVariant = Color(COLOR_SECONDARY_VARIANT_LIGHT),
         background = Color.White,
         surface = Color.White,
-        error = Color(0xFFB00020),
+        error = Color(COLOR_ERROR),
         onPrimary = Color.White,
         onSecondary = Color.Black,
         onBackground = Color.Black,
@@ -21,19 +28,25 @@ object BlityNewsColors {
         onError = Color.White
     )
 
+    private const val COLOR_PRIMARY_DARK = 0xFF7dccff
+    private const val COLOR_PRIMARY_VARIANT_DARK = 0xFF3a9cff
+    private const val COLOR_SECONDARY_DARK = 0xFFffffe6
+    private const val COLOR_SECONDARY_VARIANT_DARK = 0xFFffe0b4
+    private const val COLOR_BACKGROUND_DARK = 0xFF343434
+    private const val COLOR_SURFACE_DARK = 0xFF343434
+
     val darkColors = darkColors(
-        primary = Color(0xFF7dccff),
-        primaryVariant = Color(0xFF3a9cff),
-        secondary = Color(0xFFffffe6),
-        secondaryVariant = Color(0xFFffe0b4),
-        background = Color(0xFF343434),
-        surface = Color(0xFF343434),
-        error = Color(0xFFB00020),
+        primary = Color(COLOR_PRIMARY_DARK),
+        primaryVariant = Color(COLOR_PRIMARY_VARIANT_DARK),
+        secondary = Color(COLOR_SECONDARY_DARK),
+        secondaryVariant = Color(COLOR_SECONDARY_VARIANT_DARK),
+        background = Color(COLOR_BACKGROUND_DARK),
+        surface = Color(COLOR_SURFACE_DARK),
+        error = Color(COLOR_ERROR),
         onPrimary = Color.Black,
         onSecondary = Color.Black,
         onBackground = Color.White,
         onSurface = Color.White,
         onError = Color.White
     )
-
 }
